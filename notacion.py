471369acf939dc92efa1612acce4549e93423427
Marcos Bujosa <mbujosab@ucm.es>
2020-01-20 18:29:57 +0100

    Varios cambios

--- notacion.py
+++ notacion.py
@@ -3276,7 +3276,7 @@ class InvMat(Matrix):
         A          = Matrix(data)        
         if A.m != A.n:
             raise ValueError('Matrix no cuadrada')
-        M          = EGJCN(A)
+        M          = EGOCN(EGC(A)) # EGJCN(A)
         if M.rank < A.n:
             raise ArithmeticError('Matrix singular')        
         Inv        = I(A.n) & T(M.pasos[1])  
@@ -3818,7 +3818,7 @@ class Homogenea:
             return tex
         
         A     = Matrix(data)
-        L     = EGCsd( A )
+        L     = EGCsd( A )  # EGJCN( A )
         E     = I(A.n) & T(L.pasos[1])
         base  = [Vector(E|j) for j in range(1, L.n+1) if Vector(L|j).esNulo()]
         dim   = len(base)
@@ -3884,7 +3884,7 @@ class SEL:
                 display(Math(tex))
             return tex
         
-        L     = EGCsd( A )
+        L     = EGCsd( A )  # EGJCN( A )
         E     = I(A.n) & T(L.pasos[1])
         base  = [Vector(E|j) for j in range(1, L.n+1) if Vector(L|j).esNulo()]
         dim   = len(base)
@@ -3909,6 +3909,359 @@ class SEL:
         self.tex   = tex({A.m,A.m+A.n}|BM|{A.n}, self.pasos)
         self.eafin = EAfin(self.sgen,self.solP)
         
+    def EcParametricas(self):
+        """Representación paramétrica del SubEspacio"""
+        return '\\left\\{ \\boldsymbol{x}\\in\\mathbb{R}^' \
+          + latex(self.eafin.Rn) \
+          + '\ \\left|\ \\exists\\boldsymbol{p}\\in\\mathbb{R}^' \
+          + latex(len(self.sgen)) \
+          + '\ \\text{tal que}\ \\boldsymbol{x}= '\
+          + latex(self.solP) + '+' \
+          + latex(Matrix(self.sgen)) \
+          + '\\boldsymbol{p}\\right. \\right\\}' \
+       
+    def __repr__(self):
+        """Muestra el Espacio Nulo de una matriz en su representación python"""
+        return repr(self.solP) + ' + Combinaciones lineales de (' + repr(self.sgen) + ')'
+
+    def _repr_html_(self):
+        """Construye la representación para el entorno jupyter notebook"""
+        return html(self.latex())
+
+    def latex(self):
+        """ Construye el comando LaTeX para la solución de un Sistema Homogéneo"""
+        if self.determinado:
+            return '\\text{Tiene solución única:  }\\boldsymbol{x}=' + latex(self.solP) 
+        else:
+            return '\\text{Conjunto de vectores: }' + self.EcParametricas()
+              
+class SELM:
+    def __init__(self, A, b, rep=0):
+        """Resuelve un Sistema de Ecuaciones Lineales
+    
+        y muestra los pasos para encontrarlo"""
+        def tex(data, pasos, TexPasosPrev=[]):
+            def PasosYEscritura(data,pasos,TexPasosPrev=[]):
+                """Escribe en LaTeX los pasos efectivos dados"""
+                A   = Matrix(data);  p   = [[],[]]
+                tex = latex(data) if len(TexPasosPrev)==0 else TexPasosPrev
+                for l in range(0,2):
+                    p[l] = [ T([j for j in pasos[l][i].t if (isinstance(j,set) and len(j)>1)   \
+                                        or (isinstance(j,tuple) and len(j)==3 and j[0]!=0)     \
+                                        or (isinstance(j,tuple) and len(j)==2 and j[0]!=1) ])  \
+                                                                for i in range(0,len(pasos[l])) ]
+                    p[l]   = [ t for t in p[l] if len(t.t)!=0]  # quitamos abreviaturas vacías     
+                    if l==0:
+                        for i in reversed(range(0,len(p[l]))):
+                            tex += '\\xrightarrow[' + latex(p[l][i]) + ']{}'
+                            if isinstance (data, Matrix):
+                                         tex += latex( p[l][i] & A )
+                            elif isinstance (data, BlockMatrix):
+                                         tex += latex( key(data.lm)|(p[l][i] & A)|key(data.ln) )
+                    if l==1:
+                        for i in range(0,len(p[l])):
+                            tex += '\\xrightarrow{' + latex(p[l][i]) + '}'
+                            if isinstance (data, Matrix):
+                                         tex += latex( A & p[l][i] )
+                            elif isinstance (data, BlockMatrix):
+                                         tex += latex( key(data.lm)|(A & p[l][i])|key(data.ln) )
+                return tex
+            tex     = PasosYEscritura(data, pasos, TexPasosPrev)
+            if rep:  
+                from IPython.display import display, Math
+                display(Math(tex))
+            return tex
+        
+        MA = Matrix( BlockMatrix([ [A, -Matrix([b])] ]) )
+        LA = RGCsd ( Matrix(MA) )
+        #LA = RGCsd ( Matrix(BlockMatrix([[MA],[I(MA.n)]]) ) )
+
+        if not (LA|0).esNulo():
+            BM       = Matrix(BlockMatrix([[MA],[I(MA.n)]]))
+            self.tex = tex( {A.m,A.m+A.n}|BM|{A.n} , [[], LA.pasos[1]] )
+            raise ArithmeticError('No hay solución: Sistema incompatible')
+            
+        EA    = I(MA.n) & T(LA.pasos[1])
+        E     = list(range(1,A.n+1))|EA
+        base  = [Vector(E|j) for j in range(1, E.n+1) if Vector(LA|j).esNulo()]
+        dim   = len(base)
+        self.sgen        = Sistema(base) if dim else Sistema([V0(A.n)])
+        self.determinado = (dim == 0)
+
+        Normalizacion    = T([(Fraction(1, 0|EA|0 ), MA.n)])
+        EA               = EA & Normalizacion
+        self.solP        = list(range(1,A.n+1))|EA|0 
+
+        self.pasos = [ [], LA.pasos[1] + [Normalizacion] ]
+        BM         = Matrix(BlockMatrix([ [MA],[I(MA.n)] ]))
+        self.tex   = tex({A.m,A.m+A.n}|BM|{A.n}, self.pasos)
+        self.eafin = EAfin(self.sgen,self.solP)
+        
+    def EcParametricas(self):
+        """Representación paramétrica del SubEspacio"""
+        return '\\left\\{ \\boldsymbol{x}\\in\\mathbb{R}^' \
+          + latex(self.eafin.Rn) \
+          + '\ \\left|\ \\exists\\boldsymbol{p}\\in\\mathbb{R}^' \
+          + latex(len(self.sgen)) \
+          + '\ \\text{tal que}\ \\boldsymbol{x}= '\
+          + latex(self.solP) + '+' \
+          + latex(Matrix(self.sgen)) \
+          + '\\boldsymbol{p}\\right. \\right\\}' \
+       
+    def __repr__(self):
+        """Muestra el Espacio Nulo de una matriz en su representación python"""
+        return repr(self.solP) + ' + Combinaciones lineales de (' + repr(self.sgen) + ')'
+
+    def _repr_html_(self):
+        """Construye la representación para el entorno jupyter notebook"""
+        return html(self.latex())
+
+    def latex(self):
+        """ Construye el comando LaTeX para la solución de un Sistema Homogéneo"""
+        if self.determinado:
+            return '\\text{Tiene solución única:  }\\boldsymbol{x}=' + latex(self.solP) 
+        else:
+            return '\\text{Conjunto de vectores: }' + self.EcParametricas()
+              
+class SELB:
+    def __init__(self, A, b, rep=0):
+        """Resuelve un Sistema de Ecuaciones Lineales
+    
+        y muestra los pasos para encontrarlo"""
+        def tex(data, pasos, TexPasosPrev=[]):
+            def PasosYEscritura(data,pasos,TexPasosPrev=[]):
+                """Escribe en LaTeX los pasos efectivos dados"""
+                A   = Matrix(data);  p   = [[],[]]
+                tex = latex(data) if len(TexPasosPrev)==0 else TexPasosPrev
+                for l in range(0,2):
+                    p[l] = [ T([j for j in pasos[l][i].t if (isinstance(j,set) and len(j)>1)   \
+                                        or (isinstance(j,tuple) and len(j)==3 and j[0]!=0)     \
+                                        or (isinstance(j,tuple) and len(j)==2 and j[0]!=1) ])  \
+                                                                for i in range(0,len(pasos[l])) ]
+                    p[l]   = [ t for t in p[l] if len(t.t)!=0]  # quitamos abreviaturas vacías     
+                    if l==0:
+                        for i in reversed(range(0,len(p[l]))):
+                            tex += '\\xrightarrow[' + latex(p[l][i]) + ']{}'
+                            if isinstance (data, Matrix):
+                                         tex += latex( p[l][i] & A )
+                            elif isinstance (data, BlockMatrix):
+                                         tex += latex( key(data.lm)|(p[l][i] & A)|key(data.ln) )
+                    if l==1:
+                        for i in range(0,len(p[l])):
+                            tex += '\\xrightarrow{' + latex(p[l][i]) + '}'
+                            if isinstance (data, Matrix):
+                                         tex += latex( A & p[l][i] )
+                            elif isinstance (data, BlockMatrix):
+                                         tex += latex( key(data.lm)|(A & p[l][i])|key(data.ln) )
+                return tex
+            tex     = PasosYEscritura(data, pasos, TexPasosPrev)
+            if rep:  
+                from IPython.display import display, Math
+                display(Math(tex))
+            return tex
+        filasA = list(range(1,A.m+1))
+        filasE = list(range(1,A.n+1))
+        
+        MA = Matrix( BlockMatrix([ [A, -Matrix([b])] ]) )
+        LA = RGCsd ( Matrix(BlockMatrix([[MA],[I(MA.n)]]) ) )
+        if not (filasA|LA|0).esNulo():
+            self.tex = tex( {A.m,A.m+A.n}|MA|{A.n} , [[], LA.pasos[1]] )
+            raise ArithmeticError('No hay solución: Sistema incompatible')
+            
+        EA    = I(MA.n) & T(LA.pasos[1])
+        E     = filasE|EA|filasE
+        base  = [Vector(E|j) for j in range(1, E.n+1) if (filasA|LA|j).esNulo()]
+        dim   = len(base)
+        self.sgen        = Sistema(base) if dim else Sistema([V0(A.n)])
+        self.determinado = (dim == 0)
+
+        Normalizacion    = T([(Fraction(1, 0|EA|0 ), MA.n)])
+        EA               = EA & Normalizacion
+        self.solP        = filasE|EA|0 
+
+        self.pasos = [ [], LA.pasos[1] + [Normalizacion] ]
+        BM         = Matrix(BlockMatrix([ [MA],[I(MA.n)] ]))
+        self.tex   = tex({A.m,A.m+A.n}|BM|{A.n}, self.pasos)
+        self.eafin = EAfin(self.sgen,self.solP)
+        
+    def EcParametricas(self):
+        """Representación paramétrica del SubEspacio"""
+        return '\\left\\{ \\boldsymbol{x}\\in\\mathbb{R}^' \
+          + latex(self.eafin.Rn) \
+          + '\ \\left|\ \\exists\\boldsymbol{p}\\in\\mathbb{R}^' \
+          + latex(len(self.sgen)) \
+          + '\ \\text{tal que}\ \\boldsymbol{x}= '\
+          + latex(self.solP) + '+' \
+          + latex(Matrix(self.sgen)) \
+          + '\\boldsymbol{p}\\right. \\right\\}' \
+       
+    def __repr__(self):
+        """Muestra el Espacio Nulo de una matriz en su representación python"""
+        return repr(self.solP) + ' + Combinaciones lineales de (' + repr(self.sgen) + ')'
+
+    def _repr_html_(self):
+        """Construye la representación para el entorno jupyter notebook"""
+        return html(self.latex())
+
+    def latex(self):
+        """ Construye el comando LaTeX para la solución de un Sistema Homogéneo"""
+        if self.determinado:
+            return '\\text{Tiene solución única:  }\\boldsymbol{x}=' + latex(self.solP) 
+        else:
+            return '\\text{Conjunto de vectores: }' + self.EcParametricas()
+              
+class SELS:
+    def __init__(self, A, b, rep=0):
+        """Resuelve un Sistema de Ecuaciones Lineales
+    
+        y muestra los pasos para encontrarlo"""
+        def tex(data, pasos, TexPasosPrev=[]):
+            def PasosYEscritura(data,pasos,TexPasosPrev=[]):
+                """Escribe en LaTeX los pasos efectivos dados"""
+                A   = Matrix(data);  p   = [[],[]]
+                tex = latex(data) if len(TexPasosPrev)==0 else TexPasosPrev
+                for l in range(0,2):
+                    p[l] = [ T([j for j in pasos[l][i].t if (isinstance(j,set) and len(j)>1)   \
+                                        or (isinstance(j,tuple) and len(j)==3 and j[0]!=0)     \
+                                        or (isinstance(j,tuple) and len(j)==2 and j[0]!=1) ])  \
+                                                                for i in range(0,len(pasos[l])) ]
+                    p[l]   = [ t for t in p[l] if len(t.t)!=0]  # quitamos abreviaturas vacías     
+                    if l==0:
+                        for i in reversed(range(0,len(p[l]))):
+                            tex += '\\xrightarrow[' + latex(p[l][i]) + ']{}'
+                            if isinstance (data, Matrix):
+                                         tex += latex( p[l][i] & A )
+                            elif isinstance (data, BlockMatrix):
+                                         tex += latex( key(data.lm)|(p[l][i] & A)|key(data.ln) )
+                    if l==1:
+                        for i in range(0,len(p[l])):
+                            tex += '\\xrightarrow{' + latex(p[l][i]) + '}'
+                            if isinstance (data, Matrix):
+                                         tex += latex( A & p[l][i] )
+                            elif isinstance (data, BlockMatrix):
+                                         tex += latex( key(data.lm)|(A & p[l][i])|key(data.ln) )
+                return tex
+            tex     = PasosYEscritura(data, pasos, TexPasosPrev)
+            if rep:  
+                from IPython.display import display, Math
+                display(Math(tex))
+            return tex
+        A      = Matrix(A)
+        filasA = list(range(1,A.m+1))
+        filasE = columnasE = list(range(1,A.n+1))
+
+        MA = A if b.esNulo() else Matrix( BlockMatrix([ [A, Matrix([-b])] ]) )
+        BM = Matrix( BlockMatrix([ [MA], [I(MA.n)] ]) )
+
+        LA = RGCsd ( BM )
+
+        if not b.esNulo() and not (filasA|LA|0).esNulo():
+            self.tex = tex( {A.m, A.m+A.n} | BM | {A.n}, LA.pasos )
+            raise ArithmeticError('No hay solución: Sistema incompatible')
+            
+        EA        = I(MA.n) & T(LA.pasos[1])
+        Normaliza = T([])    if b.esNulo() else  T([(Fraction(1,0|EA|0),MA.n)])
+        EA        = EA & Normaliza
+        self.solP = V0(MA.n) if b.esNulo() else  filasE|EA|0
+
+        E                = filasE| EA |columnasE
+        base             = [ (E|j) for j in columnasE if (filasA|LA|j).esNulo()]
+        self.determinado = (len(base) == 0)
+        self.sgen        = Sistema([V0(A.n)]) if self.determinado else Sistema(base)
+
+        self.eafin       = EAfin(self.sgen,self.solP)
+
+        self.pasos = [ [], LA.pasos[1] + [Normaliza] ]
+        self.tex   = tex( {A.m, A.m+A.n} | BM | {A.n}, self.pasos )
+    def EcParametricas(self):
+        """Representación paramétrica del SubEspacio"""
+        return '\\left\\{ \\boldsymbol{x}\\in\\mathbb{R}^' \
+          + latex(self.eafin.Rn) \
+          + '\ \\left|\ \\exists\\boldsymbol{p}\\in\\mathbb{R}^' \
+          + latex(len(self.sgen)) \
+          + '\ \\text{tal que}\ \\boldsymbol{x}= '\
+          + latex(self.solP) + '+' \
+          + latex(Matrix(self.sgen)) \
+          + '\\boldsymbol{p}\\right. \\right\\}' \
+       
+    def __repr__(self):
+        """Muestra el Espacio Nulo de una matriz en su representación python"""
+        return repr(self.solP) + ' + Combinaciones lineales de (' + repr(self.sgen) + ')'
+
+    def _repr_html_(self):
+        """Construye la representación para el entorno jupyter notebook"""
+        return html(self.latex())
+
+    def latex(self):
+        """ Construye el comando LaTeX para la solución de un Sistema Homogéneo"""
+        if self.determinado:
+            return '\\text{Tiene solución única:  }\\boldsymbol{x}=' + latex(self.solP) 
+        else:
+            return '\\text{Conjunto de vectores: }' + self.EcParametricas()
+              
+class SELGJ:
+    def __init__(self, A, b, rep=0):
+        """Resuelve un Sistema de Ecuaciones Lineales
+    
+        y muestra los pasos para encontrarlo"""
+        def tex(data, pasos, TexPasosPrev=[]):
+            def PasosYEscritura(data,pasos,TexPasosPrev=[]):
+                """Escribe en LaTeX los pasos efectivos dados"""
+                A   = Matrix(data);  p   = [[],[]]
+                tex = latex(data) if len(TexPasosPrev)==0 else TexPasosPrev
+                for l in range(0,2):
+                    p[l] = [ T([j for j in pasos[l][i].t if (isinstance(j,set) and len(j)>1)   \
+                                        or (isinstance(j,tuple) and len(j)==3 and j[0]!=0)     \
+                                        or (isinstance(j,tuple) and len(j)==2 and j[0]!=1) ])  \
+                                                                for i in range(0,len(pasos[l])) ]
+                    p[l]   = [ t for t in p[l] if len(t.t)!=0]  # quitamos abreviaturas vacías     
+                    if l==0:
+                        for i in reversed(range(0,len(p[l]))):
+                            tex += '\\xrightarrow[' + latex(p[l][i]) + ']{}'
+                            if isinstance (data, Matrix):
+                                         tex += latex( p[l][i] & A )
+                            elif isinstance (data, BlockMatrix):
+                                         tex += latex( key(data.lm)|(p[l][i] & A)|key(data.ln) )
+                    if l==1:
+                        for i in range(0,len(p[l])):
+                            tex += '\\xrightarrow{' + latex(p[l][i]) + '}'
+                            if isinstance (data, Matrix):
+                                         tex += latex( A & p[l][i] )
+                            elif isinstance (data, BlockMatrix):
+                                         tex += latex( key(data.lm)|(A & p[l][i])|key(data.ln) )
+                return tex
+            tex     = PasosYEscritura(data, pasos, TexPasosPrev)
+            if rep:  
+                from IPython.display import display, Math
+                display(Math(tex))
+            return tex
+        A      = Matrix(A)
+        filasA = list(range(1,A.m+1))
+        filasE = columnasE = list(range(1,A.n+1))
+
+        MA = A if b.esNulo() else Matrix( BlockMatrix([ [A, Matrix([-b])] ]) )
+        BM = Matrix( BlockMatrix([ [MA], [I(MA.n)] ]) )
+
+        LA = EGJCN ( BM )
+
+        if not b.esNulo() and not (filasA|LA|0).esNulo():
+            self.tex = tex( {A.m, A.m+A.n} | BM | {A.n}, LA.pasos )
+            raise ArithmeticError('No hay solución: Sistema incompatible')
+            
+        EA        = I(MA.n) & T(LA.pasos[1])
+        Normaliza = T([])    if b.esNulo() else  T([(Fraction(1,0|EA|0),MA.n)])
+        EA        = EA & Normaliza
+        self.solP = V0(MA.n) if b.esNulo() else  filasE|EA|0
+
+        E                = filasE| EA |columnasE
+        base             = [ (E|j) for j in columnasE if (filasA|LA|j).esNulo()]
+        self.determinado = (len(base) == 0)
+        self.sgen        = Sistema([V0(A.n)]) if self.determinado else Sistema(base)
+
+        self.eafin       = EAfin(self.sgen,self.solP)
+
+        self.pasos = [ [], LA.pasos[1] + [Normaliza] ]
+        self.tex   = tex( {A.m, A.m+A.n} | BM | {A.n}, self.pasos )
     def EcParametricas(self):
         """Representación paramétrica del SubEspacio"""
         return '\\left\\{ \\boldsymbol{x}\\in\\mathbb{R}^' \
